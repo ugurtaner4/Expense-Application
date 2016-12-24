f63083e4502c7b7f92599b8b86ed18587e5b59d0
uğur taner <ugurtaner94@hotmail.com>
2016-12-24 11:50:08 +0200

    tablolar oluşturuldu model olusturuldu sorguda hata var kütüphanelerle ilgili olabilri

---
+++ ExpenseApp.Business/Helper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+namespace ExpenseApp.Business
+{
+    
+    class Helper
+    {
+        ExpenseApp.Data.ExpenseAppEntities entity = new Data.ExpenseAppEntities();
+        public void ExpenseFetch()
+        {
+            var sql = from x in entity.Expense select x; // böyle bi hata var abi
+
+        }
+
+    }
+}
